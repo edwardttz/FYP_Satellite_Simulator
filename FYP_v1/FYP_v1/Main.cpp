33df96f14d8ea8bd9bf8e1755f31ccccb4390a64
Vatsala <vatsala94@gmail.com>
2016-10-28 18:29:04 +0800

    For the new MOI values and method of sim

--- FYP_v1/FYP_v1/Main.cpp
+++ FYP_v1/FYP_v1/Main.cpp
@@ -24,36 +24,36 @@ int main(void)
 	*/
 
 	SpacecraftDynamics s1;
-	s1.setMOIValues(0.25, 0.15, 0.35);
+	s1.setMOIValues(0.5, 0.3, 0.7);
 	s1.setTorque(0.02, 0.02, 0.02);
 	s1.setInitialW(0.0, 0.0, 0.0);
-	s1.setStepSize(0.005);
+	s1.setStepSize(0.01);
 	s1.setQuaternionInitialValues(0.01, 0.01, 0.01, 0.01);
 	s1.findConstants();
-	
-
 	//run RK4 150 times, 50 for positive torque, 50 in negative torque and 50 with no torque after application of previous torques
-	for (int i = 0; i < 200; i++) 
+	for (int i = 0; i < 18000; i++) 
 	{
-		//Switch torque values after 50 iterations
-		if(i == 49) 
+		//Switch torque values after numberofiterations/timestep iterations
+		if(i == 2999) 
 		{ 
-			s1.setTorque(-0.02, -0.02, -0.02);
-		}
-
-		//no more torque after 100 iterations
-		if (i == 99) 
-		{
 			s1.setTorque(0.0, 0.0, 0.0);
 		}
 
+		//no more torque after 100 iterations
+		if (i == 5999) 
+		{
+			s1.setTorque(-0.02, -0.02, -0.02);
+		}
+
+		if (i == 8999)
+		{
+			s1.setTorque(0.0, 0.0, 0.0);
+		}
 		///Find acceleration, next velocity and store all values in text file
 		s1.findAcc();
 		s1.storeValues();
 		s1.findNextW();
 		s1.findNextQuaternion();
 	} 
-
 	system("PAUSE");
-
 }
