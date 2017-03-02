5f6dbe5ff58d8388225934b14bf51ce28b7d2070
Vatsala <vatsala94@gmail.com>
2017-03-02 15:21:45 +0800

    Changed polling time to 50ms

--- Gyroscope/GyroNoiseModel/GyroNoiseModel/Main.cpp
+++ Gyroscope/GyroNoiseModel/GyroNoiseModel/Main.cpp
@@ -30,7 +30,7 @@ int main(void)
 	Also this program assumes you are running at 100ms. Please do tell me if it changes as I will have to update my side too.
 	*/
 
-	g1.setPollingTime(0.1); //Polling time by default is 100ms
+	g1.setPollingTime(0.05); //Polling time by default is 50ms
 	g1.setSamplePeriod(0.02); //sample period 1 for raw gyro values
 	g1.setFSValue(250); //Full scale of gyro by default = 250
 	g1.setMaxNoiseDensity(0.37); 
